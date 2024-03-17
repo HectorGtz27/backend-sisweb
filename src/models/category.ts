85f37ccc5d6c27b2ae06c52797c4fe39ce8002dc
“maestro-sasha-valdes” <maestro.sasha.valdes@outlook.com>
2024-03-17 00:15:21 -0700

    Add sisweb_user configuration

--- src/models/category.ts
+++ src/models/category.ts
@@ -1,4 +1,4 @@
-import {Table, Model, Column, CreatedAt, UpdatedAt, DataType, HasMany} from 'sequelize-typescript';
+import {Table, Model, Column, CreatedAt, UpdatedAt, HasMany} from 'sequelize-typescript';
 import {Optional} from 'sequelize';
 import { Product } from './product';
 
