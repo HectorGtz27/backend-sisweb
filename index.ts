ff0b52659b79cd8e7ed9457210fbf0338fc011a7
HectorGtz27 <alan.gtz51@gmail.com>
2024-04-03 22:57:26 -0700

    CORS

--- index.ts
+++ index.ts
@@ -1,21 +1,27 @@
 import express, { Express } from "express";
-import apiRouter from './src/routes';
-import connectionDB from './src/connection/connection';
-const morgan = require('morgan');
 
+import apiRouter from "./src/routes";
+
+import connectionDB from "./src/connection/connection";
+
+const morgan = require("morgan");
+
+const cors = require("cors");
 
 const app: Express = express();
+
 const port = 3000;
 
+app.use(cors({ origin: true }));
 
-app.use(morgan('dev'));
-app.use(express.json()); 
+app.use(morgan("dev"));
+
+app.use(express.json());
+
 app.use(apiRouter);
 
-connectionDB();  
+connectionDB();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
